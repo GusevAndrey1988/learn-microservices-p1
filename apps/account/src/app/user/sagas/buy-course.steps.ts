599f15485a154e4d63937dcb1f0849bec76decf8
Gusev Andrey <gusevandrey1988@gmail.com>
2025-06-26 06:08:47 +0300

    lesson11.5

---
+++ apps/account/src/app/user/sagas/buy-course.steps.ts
@@ -0,0 +1,48 @@
+import { CourseGetCourse, PaymentGenerateLink } from "@purple/contracts";
+import { UserEntity } from "../entities/user.entity";
+import { BuyCourseSagaState } from "./buy-course.state";
+import { PurchaseState } from "@purple/interfaces";
+
+export class BuyCourseSagaStateStarted extends BuyCourseSagaState {
+  public async pay(): Promise<{ paymentLink: string; user: UserEntity; }> {
+    const { course } = await this.saga
+      .rmqService.send<CourseGetCourse.Request, CourseGetCourse.Response>(
+        CourseGetCourse.topic,
+        {
+          id: this.saga.courseId
+        }
+      );
+
+    if (!course) {
+      throw new Error('Такого курса не существует');
+    }
+
+    if (course.price == 0) {
+      this.saga.setState(PurchaseState.Purchased, course.id);
+      return { paymentLink: null, user: this.saga.user };
+    }
+
+    const { paymentLink } = await this.saga
+      .rmqService.send<PaymentGenerateLink.Request, PaymentGenerateLink.Response>(
+        PaymentGenerateLink.topic,
+        {
+          courseId: course.id,
+          userId: this.saga.user.id,
+          sum: course.price
+        }
+      );
+
+    this.saga.setState(PurchaseState.WaitingForPayment, course.id);
+
+    return { paymentLink, user: this.saga.user };
+  }
+
+  public checkPayment(): Promise<{ user: UserEntity; }> {
+    throw new Error("Нельзя проверить платёж, который не начался");
+  }
+
+  public async cancel(): Promise<{ user: UserEntity; }> {
+    this.saga.setState(PurchaseState.Canceled, this.saga.courseId);
+    return { user: this.saga.user };
+  }
+}
